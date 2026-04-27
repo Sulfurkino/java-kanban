b5019f8b927b8a60004010a2b395def6971790f2
Michael <kinovar13@gmail.com>
2026-04-27 14:44:20 +0700

    did style corrections

--- src/Timetable.java
+++ src/Timetable.java
@@ -1,7 +1,7 @@
 import java.time.LocalTime;
 import java.util.*;
 
-public class    Timetable {
+public class Timetable {
     private Map<DayOfWeek, TreeMap<LocalTime, List<TrainingSession>>> timetable = new HashMap<>();
 
     public Timetable() {
@@ -30,7 +30,7 @@ public class    Timetable {
 
         //достаём значение из timetable
         TreeMap<LocalTime, List<TrainingSession>> timetableValue = timetable.get(trainingSession.getDayOfWeek());
-        if (timetableValue.containsKey(trainingSession.getStartTime())){
+        if (timetableValue.containsKey(trainingSession.getStartTime())) {
             timetableValue.get(trainingSession.getStartTime()).add(trainingSession);
         } else {
             List<TrainingSession> sessions = new ArrayList<>();
@@ -42,11 +42,11 @@ public class    Timetable {
 
     }
 
-    public  List<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, LocalTime localTime) {
+    public List<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, LocalTime localTime) {
         //как реализовать, тоже непонятно, но сложность должна быть О(1)
         TreeMap<LocalTime, List<TrainingSession>> timeListTreeMap = timetable.get(dayOfWeek);
         List<TrainingSession> trainingSessionList = timeListTreeMap.get(localTime);
-        if (trainingSessionList == null){
+        if (trainingSessionList == null) {
             return Collections.emptyList();
         }
         return List.copyOf(trainingSessionList);
